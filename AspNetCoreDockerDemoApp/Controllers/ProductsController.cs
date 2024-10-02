110d096019fac79c4f2fdaa4ee433dbc194d2c1c
Fatma-Eraslan <fatoseraslan275@gmail.com>
2024-10-02 11:53:04 +0300

    Build Context

---
+++ AspNetCoreDockerDemoApp/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using AspNetCoreDockerDemoApp.Data;
+using AspNetCoreDockerDemoApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCoreDockerDemoApp.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly OnlineShopDbContext _context;
+
+    public ProductsController(OnlineShopDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<Product>> Get()
+    {
+        return await _context.Products.ToListAsync();
+    }
+}
